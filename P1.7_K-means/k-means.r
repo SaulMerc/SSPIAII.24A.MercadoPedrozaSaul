90f4fe4afe9a03cac5cc4a0485c9ff46da5f4309
SaulMerc <saul.mercado9192@alumnos.udg.mx>
2024-03-17 13:23:19 -0600

    [P1.7] Entrega no valida, pero termimando la actividad para no dejarla incompleta (Graficas)

--- P1.7_K-means/k-means.r
+++ P1.7_K-means/k-means.r
@@ -6,6 +6,7 @@ set.seed(2002)
 df.Credit <- read.csv("SSPIAII.24A.MercadoPedrozaSaul/Datasets & Lib/Credit Card_Kaggle.csv", header = TRUE, stringsAsFactors = TRUE)
 
 
+
 df.Credit$CUST_ID <- NULL
 
 corr <- cor(df.Credit)
@@ -18,11 +19,11 @@ df.Credit.new$BF <- df.Credit$BALANCE_FREQUENCY
 df.Credit.new$CSHAT <- df.Credit$CASH_ADVANCE_FREQUENCY
 df.Credit.new <- as.data.frame(df.Credit.new)
 
-#mdl.NSup <- kmeans(df.Credit.new, 5, trace = T)
+
 
 #Elbow#
 n.obs <- length(df.Credit.new$CSHAT)
-
+# 
 WCSS <- vector()
 for(i in 1:15){
     WCSS[i] <- kmeans(df.Credit.new, i)$tot.withinss
@@ -37,5 +38,77 @@ ggplot()+ geom_line(aes(x = WCSS$K, y = WCSS$WCSS))+
     ggtitle("Método del codo")+
     xlab("Iteración")+
     ylab("WCSS")+
-    theme_minimal()    
+    theme_minimal()
 
+#K5 resulta ser la mejor opcion resultante tanto en WCSS como en el metodo K-means#
+
+#Manera 1 con libreria factoextra#
+#Se utiliza principalmente para visualizar los clusters de K-means#
+#toma los resultados de los datos original como argumentos #
+install.packages("factoextra")
+library(factoextra)
+
+mdl.NSup <- kmeans(df.Credit.new, 5, trace = TRUE)
+
+fviz_cluster(mdl.NSup, data = df.Credit.new, geom = "point",
+ellipse.type= "convex")
+
+#Adaptando el gráfico del cluster de K-Means con ggpubr#
+#Utilizando ggscatter()#
+install.packages("ggpubr")
+library(ggpubr)
+
+#Reducir las dimenciones utilizando PCA#
+#Análisis de componentes principales, técnica estadística para describir#
+#un término de conjuntos de datos en tperminos de nuevas variables# 
+#no correlacionadas#
+
+#Reduccion de dimensiones#
+res.pca <- prcomp(df.Credit.new, scale = TRUE)
+
+#Coordenadas de los individuos#
+ind.coord <- as.data.frame(get_pca_ind(res.pca)$coord)
+
+#Añadir los clusters obtenidos utilizando el alg K-Means#
+ind.coord$cluster <- factor(mdl.NSup$cluster)
+
+#Añadir las caracteristicas CASH_ADVANCE_FREQUENCY, BALANCE_FREQUENCY y CASH_ADVANCE#
+ind.coord$CHA <- df.Credit$CASH_ADVANCE
+ind.coord$BF <- df.Credit$BALANCE_FREQUENCY
+ind.coord$CSHAT <- df.Credit$CASH_ADVANCE_FREQUENCY
+
+#Inspección de los datos#
+head(ind.coord)
+
+#Porcentaje de la vairanza explicada mediante dimensiones#
+#trabaja con Eigenvalor#
+eigenvalue <- round(get_eigenvalue(res.pca),1)
+variance.percent <- eigenvalue$variance.percent
+
+head(eigenvalue)
+
+#Graficacion#
+ggscatter(ind.coord, "Dim.1", "Dim.2",
+    color = "cluster", palette = "npg",
+    ellipse = TRUE, ellipse.type = "convex",
+    size = 1.5, legend = "right",
+    ggtheme = theme_minimal(), xlab = paste0("Dim 1", variance.percent[1], "%"),
+    ylab = paste0("Dim 2", variance.percent[2], "%") +
+    stat_mean(aes(color = cluster), size = 4)
+)
+
+#Modo 3 utilizando Plot cluster#
+install.packages("cluster")
+install.packages("fpc")
+library(cluster)
+library(fpc)
+
+#KM <- mdl.NSup#
+#De manera simple se puede graficar de la siguiente manera#
+plotcluster(df.Credit.new, mdl.NSup$cluster)
+#O también de la siguiente manera#
+clusplot(df.Credit.new, mdl.NSup$cluster, color = TRUE,
+        shade = TRUE, labels = 2, lines = 0)
+#Al igual que el plot, es una manera diferente de visualizar los datos del cluster#
+#conforme a la matriz de correlacion#
+with(df.Credit, pairs(df.Credit.new, col=c(1:3)[mdl.NSup$cluster]))
